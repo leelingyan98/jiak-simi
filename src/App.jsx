1aa2639f0239b6323bfb07e3b30b1e0a6cf63696
leelingyan98 <58385937+leelingyan98@users.noreply.github.com>
2024-03-23 13:00:51 +0800

    Update Route for Categories List

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,31 @@
 import './App.css';
 import Home from './components/Home';
+import NavBar from './components/NavBar';
+import CategoriesList from './components/CategoriesList';
 import CategoryList from './components/CategoryList';
 import Meal from './components/Meal';
+import SavedMealsList from './components/SavedMealsList';
 import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="container">
+      <NavBar />
       <Switch>
-      <Route path="/:category/:mealid">
+        <Route path="/categories/:category/:mealid">
           <Meal />
         </Route>
 
-        <Route path="/:category">
+        <Route path="/categories/:category">
           <CategoryList />
+        </Route>
+
+        <Route path="/categories">
+          <CategoriesList />
+        </Route>
+
+        <Route path="/saved">
+          <SavedMealsList />
         </Route>
         
         <Route path="/">
